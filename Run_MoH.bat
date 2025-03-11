e591e85fbbe2c3a2a6d0dae8853f55872c551a72
Bud Davis <jimmie.davis@uta.edu>
2025-03-11 10:57:27 -0500

    Added script to run things.

--- Run_MoH.bat
+++ Run_MoH.bat
@@ -5,16 +5,32 @@ setlocal
 set "IP1=169.254.208.9"
 set "IP2=169.254.172.31"
 
-:: Connect to the first remote machine and change to the specified directory, then run MoH_VR.exe
-start cmd.exe /k "psexec -i \\crewchief "C:\Users\admin\Desktop\MoH\Windows\MoH_VR.exe""
+ 
+if [%1]==[] (set /A N=3) else (set /A N="%1")
+  
+if %N%==3 start cmd.exe /c "psexec -i \\copilot "C:\Users\test\Desktop\MoH\Windows\MoH_VR.exe"   "
+ 
+if %N% GEQ 1 start cmd.exe /c "psexec -i \\crewchief "C:\Users\admin\Desktop\MoH\Windows\MoH_VR.exe""
+ 
+if %N% GEQ 0 start cmd.exe /c "C:\Users\admin\Desktop\Windows\MoH_VR.exe  %N% "
+ 
+echo %N%
+ 
 
 
+
+
+
+:: Connect to the first remote machine and change to the specified director3y, then run MoH_VR.exe
+::     start cmd.exe /c "psexec -i \\crewchief "C:\Users\admin\Desktop\MoH\Windows\MoH_VR.exe""
 
 :: Connect to the second remote machine and change to the specified directory, then run MoH_VR.exe
-start cmd.exe /k "psexec -i \\copilot "C:\Users\test\Desktop\MoH\Windows\MoH_VR.exe""
+::      start cmd.exe /c "psexec -i \\copilot "C:\Users\test\Desktop\MoH\Windows\MoH_VR.exe""
 
+::start cmd.exe /c "psexec -i \\medic "C:\Users\admin\Desktop\Windows\MoH_VR.exe""
 
-start cmd.exe /k "C:\Users\admin\Desktop\Windows\MoH_VR.exe"
+::       start cmd.exe /c "C:\Users\admin\Desktop\Windows\MoH_VR.exe"
+
 
 
 ::psexec -s -i 0 \\169.254.208.9 "C:\Users\admin\Desktop\test.bat"
